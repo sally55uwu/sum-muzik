a6cc66a71614407bda30bd4518a590839ddf0604
sally55uwu <kokolizo22@outlook.com>
2025-01-20 16:18:53 -0500

    Adding placeholder menu items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+   
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        let command = &args[1];
+        match &command[..] {
+            "add" => println!("adding to playlist"),
+            "delete" => println!("delete"),  
+            "play" => println!("play the song"),
+            "pause" => println!("pause the song"),
+            "vu" => println!("volume up"),
+            "vd" => println!("volume down"),
+            "exit" => println!("exit the app"),
+            _ => println!("nothing was typed"),
+            /* "help" | "--help" | "-h" | _ => help(), */
+        }
+    } else {
+    }
 }
